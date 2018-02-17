e07a713af77fe29771d1ffdf9ea15df4640264e9
Reymart King <reymartking@gmail.com>
2018-02-17 14:29:30 +0800

    Quiz 2

---
+++ Quiz 2/aplimat-labs/aplimat-labs/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using aplimat_labs.Utilities;
+using SharpGL;
+using SharpGL.SceneGraph.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace aplimat_labs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random r = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private CubeMesh mover = new CubeMesh(-25, 0, 0);
+        private Vector3 acceleration = new Vector3(0.01f, 0, 0);
+        private Vector3 deceleration = new Vector3(-0.5f, 0, 0);
+        private void OpenGLControl_OpenGLDraw(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            OpenGL gl = args.OpenGL;
+
+            // Clear The Screen And The Depth Buffer
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            // Move Left And Into The Screen
+            gl.LoadIdentity();
+            gl.Translate(0.0f, 0.0f, -50.0f);
+
+            mover.Draw(gl);
+
+            if(mover.Position.x <= 25.0f)
+            {
+                mover.Velocity += acceleration;
+                mover.Velocity.Clamp(2.0f);
+            }
+            else if (mover.Velocity.x >= 0.0f)
+            {
+                mover.Velocity += deceleration;
+                mover.Velocity.Clamp(0.0f);
+            }
+            
+
+            gl.DrawText(20, 20, 1, 0, 0, "Arial", 25, mover.Velocity.x + " ");
+            return;
+            //mousePos.Normalize();
+            //mousePos *= 10;
+
+            //Line1
+            //gl.LineWidth(30.0f);
+            //gl.Begin(OpenGL.GL_LINE_STRIP);
+            //gl.Color(1.0f, 0.0f, 0.0f);
+            //gl.Vertex(0, 0, 0);
+            //gl.Vertex(mousePos.x, mousePos.y, 0);
+            //gl.End();
+
+            //gl.LineWidth(3.0f);
+            //gl.Begin(OpenGL.GL_LINE_STRIP);
+            //gl.Color(1.0f, 1.0f, 1.0f);
+            //gl.Vertex(0, 0, 0);
+            //gl.Vertex(mousePos.x, mousePos.y, 0);
+            //gl.End();
+            ////Line 2
+            //gl.LineWidth(30.0f);
+            //gl.Begin(OpenGL.GL_LINE_STRIP);
+            //gl.Color(0.0f, 1.0f, 0.0f);
+            //gl.Vertex(50, 20, 0);
+            //gl.Vertex(mousePos.x, mousePos.y, 0);
+            //gl.End();
+            
+            //gl.LineWidth(3.0f);
+            //gl.Begin(OpenGL.GL_LINE_STRIP);
+            //gl.Color(1.0f, 1.0f, 1.0f);
+            //gl.Vertex(50, 20, 0);
+            //gl.Vertex(mousePos.x, mousePos.y, 0);
+            //gl.End();
+            ////Line 3
+            //gl.LineWidth(30.0f);
+            //gl.Begin(OpenGL.GL_LINE_STRIP);
+            //gl.Color(0.0f, 0.0f, 1.0f);
+            //gl.Vertex(-50, -20, 0);
+            //gl.Vertex(mousePos.x, mousePos.y, 0);
+            //gl.End();
+
+            //gl.LineWidth(3.0f);
+            //gl.Begin(OpenGL.GL_LINE_STRIP);
+            //gl.Color(1.0f, 1.0f, 1.0f);
+            //gl.Vertex(-50, -20, 0);
+            //gl.Vertex(mousePos.x, mousePos.y, 0);
+            //gl.End();
+        }
+
+        float rotation = 0;
+        private Vector3 mousePos = new Vector3();
+
+        private void OpenGLControl_OpenGLInitialized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            OpenGL gl = args.OpenGL;
+
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+
+            float[] global_ambient = new float[] { 0.5f, 0.5f, 0.5f, 1.0f };
+            float[] light0pos = new float[] { 0.0f, 5.0f, 10.0f, 1.0f };
+            float[] light0ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
+            float[] light0diffuse = new float[] { 0.3f, 0.3f, 0.3f, 1.0f };
+            float[] light0specular = new float[] { 0.8f, 0.8f, 0.8f, 1.0f };
+
+            float[] lmodel_ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
+            gl.LightModel(OpenGL.GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+
+            gl.LightModel(OpenGL.GL_LIGHT_MODEL_AMBIENT, global_ambient);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_POSITION, light0pos);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_AMBIENT, light0ambient);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_DIFFUSE, light0diffuse);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_SPECULAR, light0specular);
+            gl.Disable(OpenGL.GL_LIGHTING);
+            gl.Disable(OpenGL.GL_LIGHT0);
+
+            gl.ShadeModel(OpenGL.GL_SMOOTH);
+        }
+
+        private void OpenGLControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            var pos = e.GetPosition(this);
+
+            mousePos.x = (float)pos.X - (float)Width / 2.0f;
+            mousePos.y = (float)pos.Y - (float)Height / 2.0f;
+
+            mousePos.y = -mousePos.y;
+            //mousePos = new Vector3(e.GetPosition(this).X, e.GetPosition(this).Y, 0);
+
+            //mousePos.x = (float)mousePos.x - (float)Width / 2.0f;
+            //mousePos.y = (float)mousePos.y - (float)Height / 2.0f;
+
+            Console.WriteLine("Mouse X: " + mousePos.x + " Y: " + mousePos.y);
+        }
+    }
+}
---
+++ Quiz 2/aplimat-labs/aplimat-labs/Models/Vector3.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplimat_labs
+{
+    public class Vector3
+    {
+        public float x, y, z;
+
+        public Vector3()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+        public Vector3(float _x, float _y, float _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+
+        public Vector3(double _x, double _y, double _z)
+        {
+            x = (float)_x;
+            y = (float)_y;
+            z = (float)_z;
+        }
+
+        public static Vector3 operator +(Vector3 rhs, Vector3 lhs) //rhs = right hand side 
+        {
+            return new Vector3(rhs.x + lhs.x,
+                rhs.y + lhs.y,
+                rhs.z + lhs.z);
+        }
+
+        public static Vector3 operator -(Vector3 rhs, Vector3 lhs) //rhs = right hand side 
+        {
+            return new Vector3(rhs.x - lhs.x,
+                rhs.y - lhs.y,
+                rhs.z - lhs.z);
+        }
+
+        public static Vector3 operator *(Vector3 rhs, float scalar)
+        {
+            return new Vector3(rhs.x * scalar,
+                rhs.y * scalar,
+                rhs.z * scalar);
+        }
+        public float GetMagnitude()
+        {
+            return (float)Math.Sqrt((x * x) + (y * y) + (z * z));
+        }
+
+        public Vector3 Normalize()
+        {
+            float length = GetMagnitude();
+            this.x /= length;
+            this.y /= length;
+            this.z /= length;
+
+            return new Vector3(x, y, z);
+        }
+        //public void Clamp(Vector3 limit)
+        //{
+        //    if (this.x >= limit.x) this.x = limit.x;
+        //    if (this.y >= limit.y) this.y = limit.y;
+        //    if (this.z >= limit.z) this.z = limit.z;
+        //}
+        public void Clamp(float limit)
+        {
+            if (this.x >= limit) this.x = limit;
+            if (this.y >= limit) this.y = limit;
+            if (this.z >= limit) this.z = limit;
+        }
+    }
+}
