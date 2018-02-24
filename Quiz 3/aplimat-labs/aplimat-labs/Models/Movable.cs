84ad66db093365023ceb8c025b3e4502c353fa41
Reymart King <reymartking@gmail.com>
2018-02-24 14:15:34 +0800

    Quiz 3 - Forces (No Good)

---
+++ Quiz 3/aplimat-labs/aplimat-labs/Models/Movable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplimat_labs
+{
+    public class Movable
+    {
+        public Vector3 Position;
+        public Vector3 Velocity;
+        public Vector3 Acceleration;
+        public float Mass = 1;
+
+        public Movable()
+        {
+            this.Position = new Vector3();
+            this.Velocity = new Vector3();
+            this.Acceleration = new Vector3();
+        }
+
+        public void ApplyForce(Vector3 force)
+        {
+            //F = MA
+            //A = F/M
+            this.Acceleration += (force / Mass); //force accumulation
+        }
+    }
+}
